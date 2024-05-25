3245280c11bff2cb26c02563b2e1280e96116c72
vicgrabru <vgravanbru@gmail.com>
2024-05-25 18:36:31 +0200

    fix: added missing data for test suite

--- src/main/java/acme/features/manager/project/ManagerProjectDeleteService.java
+++ src/main/java/acme/features/manager/project/ManagerProjectDeleteService.java
@@ -17,13 +17,10 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import acme.client.data.datatypes.Money;
-import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
 import acme.entities.project.Project;
 import acme.entities.project.UserStoryAssign;
 import acme.roles.Manager;
-import acme.utils.MoneyExchangeRepository;
 
 @Service
 public class ManagerProjectDeleteService extends AbstractService<Manager, Project> {
@@ -31,10 +28,10 @@ public class ManagerProjectDeleteService extends AbstractService<Manager, Projec
 	// Internal state ---------------------------------------------------------
 
 	@Autowired
-	private ManagerProjectRepository	repository;
+	private ManagerProjectRepository repository;
 
-	@Autowired
-	private MoneyExchangeRepository		exchangeRepository;
+	//	@Autowired
+	//	private MoneyExchangeRepository		exchangeRepository;
 
 	// AbstractService interface ----------------------------------------------
 
@@ -88,23 +85,23 @@ public class ManagerProjectDeleteService extends AbstractService<Manager, Projec
 		this.repository.delete(object);
 	}
 
-	@Override
-	public void unbind(final Project object) {
-		assert object != null;
-
-		Money exchangedCost;
-		Dataset dataset;
-
-		dataset = super.unbind(object, "code", "title", "abstractField", "hasFatalErrors", "cost", "optionalLink", "draftMode");
-		dataset.put("masterId", object.getId());
-		dataset.put("readOnlyCode", true);
-
-		dataset.put("showExchangedCost", !this.exchangeRepository.findSystemCurrency().equals(object.getCost().getCurrency()));
-
-		exchangedCost = this.exchangeRepository.exchangeMoney(object.getCost());
-		dataset.put("exchangedCost", exchangedCost);
-
-		super.getResponse().addData(dataset);
-	}
+	//	@Override
+	//	public void unbind(final Project object) {
+	//		assert object != null;
+	//
+	//		Money exchangedCost;
+	//		Dataset dataset;
+	//
+	//		dataset = super.unbind(object, "code", "title", "abstractField", "hasFatalErrors", "cost", "optionalLink", "draftMode");
+	//		dataset.put("masterId", object.getId());
+	//		dataset.put("readOnlyCode", true);
+	//
+	//		dataset.put("showExchangedCost", !this.exchangeRepository.findSystemCurrency().equals(object.getCost().getCurrency()));
+	//
+	//		exchangedCost = this.exchangeRepository.exchangeMoney(object.getCost());
+	//		dataset.put("exchangedCost", exchangedCost);
+	//
+	//		super.getResponse().addData(dataset);
+	//	}
 
 }
