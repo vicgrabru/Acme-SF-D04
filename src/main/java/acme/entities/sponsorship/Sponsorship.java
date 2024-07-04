0186945e6938b8a8eeb5e8412db273155480a1c0
vicgrabru <vgravanbru@gmail.com>
2024-07-04 08:42:16 +0200

    fixed indexes for s4 entities

--- src/main/java/acme/entities/sponsorship/Sponsorship.java
+++ src/main/java/acme/entities/sponsorship/Sponsorship.java
@@ -42,7 +42,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(indexes = {
-	@Index(columnList = "code"), @Index(columnList = "draftMode")
+	@Index(columnList = "code")
 })
 public class Sponsorship extends AbstractEntity {
 
